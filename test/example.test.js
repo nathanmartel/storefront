bb4270ed6cd890c8525ea56b876730825073103b
Nathan Martel <nathanmartel@gmail.com>
2020-01-22 11:42:11 -0800

    Modularized functions for use in app and test

--- test/example.test.js
+++ test/example.test.js
@@ -1,22 +1,12 @@
 // IMPORT MODULES under test here:
 import lesserPets from '../src/lesser-pets.js';
-import buildPetLi from '../src/products.js';
+import buildPetLi from '../src/builtPetLi.js';
 
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-
-    //Act 
-    // Call the function you're testing and set the result to a const
-
-    //Assert
-    // Make assertions about what is expected valid result
-    const actualResultTemp = buildPetLi(lesserPets[0]);
-    const actualResult = actualResultTemp.outerHTML;
+    const actualResult = buildPetLi(lesserPets[0]).outerHTML;
     const expectedResult = `<li class="pet-listing"><div class="image-container"><img src="../assets/goldfish.jpg" alt="A Goldfish photo"></div><div class="text-container"><h3>Goldfish</h3><p class="description">One of the most commonly kept aquarium fish.</p><p class="category">Fish</p><p class="price">$5.00</p><button value="goldfish">Add</button></div></li>`;
-
 
     assert.equal(expectedResult, actualResult);
 });
