972ed444b68392408692236bb6ee29e2d6bdbbb0
Nathan Martel <nathanmartel@gmail.com>
2020-01-22 11:29:13 -0800

    Wrote test and looping function

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
-// import example from '../src/example.js';
+import lesserPets from '../src/lesser-pets.js';
+import buildPetLi from '../src/products.js';
 
 const test = QUnit.test;
 
@@ -12,5 +13,10 @@ test('time to test a function', function(assert) {
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    const actualResultTemp = buildPetLi(lesserPets[0]);
+    const actualResult = actualResultTemp.outerHTML;
+    const expectedResult = `<li class="pet-listing"><div class="image-container"><img src="../assets/goldfish.jpg" alt="A Goldfish photo"></div><div class="text-container"><h3>Goldfish</h3><p class="description">One of the most commonly kept aquarium fish.</p><p class="category">Fish</p><p class="price">$5.00</p><button value="goldfish">Add</button></div></li>`;
+
+
+    assert.equal(expectedResult, actualResult);
 });
